f4138771c6b928127bf754814b9c394e84db57e3
Ritesh <riteshsaini331@gmail.com>
2022-01-06 18:52:58 +0530

    Button added

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,44 +1,72 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:swastha/utils/styles.dart';
 import 'package:swastha/widgets/circularLoginComponent.dart';
-import 'package:swastha/widgets/myTextField.dart';
+import 'package:swastha/widgets/round_button.dart';
 
 class Login extends StatelessWidget {
-  static const String id ='Login';
+  static const String id = 'Login';
 
   const Login({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-        home:Scaffold(
-          backgroundColor: const Color(0xffF4F6FA),
-          appBar: AppBar(
-            title:Text("Login"),
-            backgroundColor:const Color(0xff7A64E4),
-          ),
-          body: Center(
-            child: Column(
-              children: <Widget>[
-                Image.asset('assets/images/logo.png',height: 100,width: 100,),
-                const myTextField(text:"Username",icon:Icons.people),
-                const   myTextField(text:"Password",icon:Icons.lock),
-                SizedBox.fromSize(size: Size(0, 30)),
-                const Text("Sign in with",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w900),),
-                Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const <Widget>[
-                      circularLoginOption(),
-                      circularLoginOption(),
-                      circularLoginOption()
-                    ],
-                  ),
-                ),
-
-              ],
+    return MaterialApp(
+        home: Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Image.asset(
+              'assets/images/logo.png',
+              height: 100,
+              width: 100,
             ),
-          ),
-        )
-    );
+            const SizedBox(
+              height: 48.0,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (value) {},
+              decoration: kTextFieldDecoration.copyWith(
+                prefixIcon: const Icon(Icons.person,color: primaryColor,),
+                  hintText: 'Enter your email',
+                  labelText: 'Enter Your Email'),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              obscureText: true,
+              onChanged: (value) {},
+              decoration: kTextFieldDecoration.copyWith(
+                  prefixIcon: const Icon(Icons.lock,color: primaryColor,),
+                  hintText: 'Enter your password',
+                  labelText: 'Enter your password'
+
+              ),
+            ),
+            const SizedBox(
+              height: 24.0,
+            ),
+            const Text(
+              "Sign in with",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+            ),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  circularLoginOption(),
+                  circularLoginOption(),
+                  circularLoginOption()
+                ],
+              ),
+            ),
+            RoundedButton(
+                title: 'Continue', colour: primaryColor, onPressed: () {})
+          ],
+        ),
+      ),
+    ));
   }
 }
--- lib/utils/styles.dart
+++ lib/utils/styles.dart
@@ -1,6 +1,24 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 const Color primaryColor = Color(0xff7A64E4);
 
 const Color secondaryColor = Color(0xff59595d);
 
+const kTextFieldDecoration = InputDecoration(
+  hintText: 'Enter a value',
+  labelStyle: TextStyle(color: primaryColor),
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color:primaryColor, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: primaryColor, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+  ),
+);
