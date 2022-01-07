69ebec94a5821610cc089d65175b25eb7ba06c2b
Vivek <thevivekbudakoti@gmail.com>
2022-01-07 19:03:15 +0530

    Slider for extra User Detail added

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -10,36 +10,6 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-  int _activeStepIndex = 0;
-
-  List<Step> stepList() =>[
-    Step(
-        state:
-        _activeStepIndex <= 0 ? StepState.editing : StepState.complete,
-        isActive: _activeStepIndex >= 0,
-        title: const Text('')
-        ,content: Container(
-      child: const Text("1st")
-    )),
-
-    Step(
-        state:
-        _activeStepIndex <= 1 ? StepState.editing : StepState.complete,
-        isActive: _activeStepIndex >= 1,
-        title: const Text(''), content: Container(
-        child: const Text("2nd")
-    )),
-
-    Step(
-        state:
-        _activeStepIndex <= 2 ? StepState.editing : StepState.complete,
-        isActive: _activeStepIndex >= 2,
-        title: const Text(''), content: Container(
-        child: const Text("3rd")
-    )),
-
-
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -47,33 +17,7 @@ class _RegisterState extends State<Register> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Stepper(
-            steps: stepList(),
-          type:StepperType.horizontal,
-            currentStep: _activeStepIndex,
-            onStepContinue: (){
-              if (_activeStepIndex < (stepList().length - 1)) {
-                setState(() {
-                  _activeStepIndex += 1;
-                });
-              } else {
-                print('Submited');
-              }
-            },
-            onStepCancel: () {
-              if (_activeStepIndex == 0) {
-                return;
-              }
-              setState(() {
-                _activeStepIndex -= 1;
-              });
-            },
-            onStepTapped: (int index) {
-              setState(() {
-                _activeStepIndex = index;
-              });
-            },
-          )
+          child: Text("Register")
         ),
       ),
     );
