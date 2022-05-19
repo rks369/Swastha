8e20ef9c67ebb5b3f0dab33670055c3d21c90bac
Ritesh <riteshsaini331@gmail.com>
2022-05-19 23:03:25 +0530

    My Account Section Done

--- lib/screens/side_drawer/my_account.dart
+++ lib/screens/side_drawer/my_account.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:swastha/Bloc/auth_cubit.dart';
 import 'package:swastha/utils/styles.dart';
 
 class MyAccount extends StatelessWidget {
@@ -6,6 +8,8 @@ class MyAccount extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final blocProvider = BlocProvider.of<AuthCubit>(context);
+
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
       floatingActionButton: FloatingActionButton(
@@ -42,7 +46,38 @@ class MyAccount extends StatelessWidget {
                 borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(30),
                     topRight: Radius.circular(30))),
-            child: Column(),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20.0,
+                ),
+                CircleAvatar(
+                  radius: 100.0,
+                  backgroundImage:
+                      Image.network(blocProvider.userModel.profileURL).image,
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                const SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  blocProvider.userModel.name,
+                  style: kHeadingTextStyle,
+                ),
+                const SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  blocProvider.userModel.mobile,
+                  style: kSubHeadingTextStyle,
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+              ],
+            ),
           )),
         ])),
       ),
