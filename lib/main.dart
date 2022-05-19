95fb1091b45a7d7c1354230effc786d5871549a2
Ritesh <riteshsaini331@gmail.com>
2022-05-19 22:48:36 +0530

    Spin Kit Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:swastha/Bloc/auth_cubit.dart';
 import 'package:swastha/screens/home.dart';
 import 'package:swastha/screens/authentication/register.dart';
 import 'package:swastha/screens/authentication/user_detail.dart';
+import 'package:swastha/utils/styles.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
