89b40a13b085d92d04b7bb400d880309e713803e
Vivek <thevivekbudakoti@gmail.com>
2022-05-20 07:31:43 +0530

    2.0

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
             }),
             builder: (context, state) {
               if (state == authstate.loggedIn) {
-                return const WaterScreen();
+                return const Home();
               } else if (state == authstate.loggedOut) {
                 return const Register();
               } else if (state == authstate.unRegistered) {
